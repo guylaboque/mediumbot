4389be18607fcda418f3667b53f9381ffe0863cc
guylaboque <82145013+guylaboque@users.noreply.github.com>
2021-08-16 23:19:25 +0200

    Create pi_run.sh

---
+++ run/pi_run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# A simple shell script to update the telegram search parameters of the bot and run it afterwards
+
+cd "$(dirname "$0")"
+cd ..
+sudo wget --output-document=scraping_config.json https://raw.githubusercontent.com/guylaboque/mediumbot/main/scraping_config.json # Pull new scraping config file
+python src/main.py # Run medium scraper bot
