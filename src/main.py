514ace0f9f9b947ecd19c866f13159082980d49f
guylaboque <82145013+guylaboque@users.noreply.github.com>
2021-08-18 20:04:24 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,37 @@
 from datetime import date, timedelta
 import logging
 import json
+import sys
 
 import mediumcrawler as mcr
 import telegramsender as tgs
 
 ## Set parameters
 #   Import scraping config
-with open('../scraping_config.json', 'r') as f:
-    config = json.load(f)
+try:
+    with open('../scraping_config.json', 'r') as f:
+        config = json.load(f)
 
-    daysSincePublishing = config['daysSincePublishing']
-    tags = config['tags']
-    keywords = config['keywords']
-    blacklist = config['blacklist']
-    likeThreshold = config['likeThreshold']
+        daysSincePublishing = config['daysSincePublishing']
+        tags = config['tags']
+        keywords = config['keywords']
+        blacklist = config['blacklist']
+        likeThreshold = config['likeThreshold']
+except Exception:
+    logging.exception("Something went wrong while reading the scraping config")
+
 
 crawlDate = date.today() - timedelta(days=daysSincePublishing)
 
 #   Import telegram bot config
-with open('../bot_config.json', 'r') as f:
-    telegramConfig = json.load(f)
+try:
+    with open('../bot_config.json', 'r') as f:
+        telegramConfig = json.load(f)
 
-    BOT_TOKEN = telegramConfig['BOT_TOKEN']
-    CHAT_ID = telegramConfig['CHAT_ID']
+        BOT_TOKEN = telegramConfig['BOT_TOKEN']
+        CHAT_ID = telegramConfig['CHAT_ID']
+except Exception:
+        logging.exception("Something went wrong while reading the bot config")
 
 #   Create telegram styling and header
 tagEmoji = '\U0001F3F7' #to set before the tag explanation
@@ -43,13 +51,20 @@ logging.info('\nBot successfully started on ' + str(date.today()))
 
 ## Crawl relevant Medium pages for articles matching the filters
 articleList = [] #initiate list for all articles in all tags
-for url in mcr.urls.getTagUrls(tags, crawlDate): #for all pages corresponding to defined tags
-    articlesInTag = mcr.crawler.getArticles(url)
-    filteredArticlesInTag = mcr.filters.filterBlacklist(mcr.filters.filterKeywords(articlesInTag, keywords), blacklist)
-    articleList += filteredArticlesInTag
+
+try:
+    for url in mcr.urls.getTagUrls(tags, crawlDate): #for all pages corresponding to defined tags
+        articlesInTag = mcr.crawler.getArticles(url)
+        filteredArticlesInTag = mcr.filters.filterBlacklist(mcr.filters.filterKeywords(articlesInTag, keywords), blacklist)
+        articleList += filteredArticlesInTag
+except Exception:
+    logging.exception("Something went wrong with the medium crawler")
 
 articleList = list(set(articleList)) #remove duplicate entries
 
 ## Send telegram message
-telegramMessageHtml = tgs.createHtmlMessage(articleList, telegramMessageHeader, likeThreshold)
-messageSent = tgs.sentTelegramMessage(BOT_TOKEN, CHAT_ID, telegramMessageHtml)
+try:
+    telegramMessageHtml = tgs.createHtmlMessage(articleList, telegramMessageHeader, likeThreshold)
+    messageSent = tgs.sentTelegramMessage(BOT_TOKEN, CHAT_ID, telegramMessageHtml)
+except Exception:
+    logging.exception("Something went wrong with the telegram sender")
