3ec08f6b63a83b83df002b9ac918cc844d0029a9
guylaboque <82145013+guylaboque@users.noreply.github.com>
2021-09-01 17:58:29 +0200

    Create __init__.py

---
+++ src/mediumcrawler/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ["listfilters", "urls", "crawler", "article", "categories"]
+
+from . import listfilters
+from . import urls
+from . import crawler
+from . import article
+from . import categories
